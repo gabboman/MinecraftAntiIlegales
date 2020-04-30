599a288ebd47d77e7c1263331233471438b00f1c
Gabriel Amador García <gabriel@titaniumsystem.es>
2020-05-01 00:01:31 +0200

    no romper endframes, bug end

--- src/main/java/com/danimania/mineanarquia/itemsilegales/Mineanarquia/ItemsIlegales.java
+++ src/main/java/com/danimania/mineanarquia/itemsilegales/Mineanarquia/ItemsIlegales.java
@@ -2,18 +2,18 @@ package com.danimania.mineanarquia.itemsilegales.Mineanarquia;
 
 import com.google.common.eventbus.Subscribe;
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.World;
 import org.bukkit.block.Barrel;
 import org.bukkit.block.Block;
 import org.bukkit.block.Chest;
 import org.bukkit.block.ShulkerBox;
-import org.bukkit.entity.Firework;
-import org.bukkit.entity.ItemFrame;
-import org.bukkit.entity.Shulker;
-import org.bukkit.entity.SplashPotion;
+import org.bukkit.entity.*;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
+import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.entity.EntityPotionEffectEvent;
 import org.bukkit.event.inventory.InventoryInteractEvent;
 import org.bukkit.event.inventory.InventoryOpenEvent;
@@ -25,14 +25,50 @@ import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.potion.Potion;
 import org.bukkit.potion.PotionData;
 import org.bukkit.potion.PotionEffect;
-
+import com.comphenix.protocol.PacketType;
+import com.comphenix.protocol.ProtocolLibrary;
+import com.comphenix.protocol.ProtocolManager;
+import com.comphenix.protocol.events.PacketAdapter;
+import com.comphenix.protocol.events.PacketContainer;
+import com.comphenix.protocol.events.PacketEvent;
+import com.comphenix.protocol.events.PacketListener;
 import java.util.List;
+import java.util.Random;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.java.JavaPlugin;
 
 public final class ItemsIlegales extends JavaPlugin implements Listener {
 
+    private ProtocolManager protocolManager;
+
+    private Random rng;
+
+
     @Override
     public void onEnable() {
+
+        this.protocolManager = ProtocolLibrary.getProtocolManager();
+        this.rng = new Random();
+
         Bukkit.getServer().getPluginManager().registerEvents(this, this);
+        // antithunder
+        this.protocolManager.addPacketListener((PacketListener)new PacketAdapter((Plugin)this, new PacketType[] { PacketType.Play.Server.NAMED_SOUND_EFFECT }) {
+            public void onPacketSending(PacketEvent event) {
+                PacketContainer packet = event.getPacket();
+                Player p = event.getPlayer();
+                String soundName = (String)packet.getStrings().read(0);
+                //if (soundName.equals("ambient.weather.thunder")) {
+                if( true ){
+                    int x = ((Integer)packet.getIntegers().read(0)).intValue() / 8;
+                    int z = ((Integer)packet.getIntegers().read(2)).intValue() / 8;
+                    int distance = ItemsIlegales.this.distanceBetweenPoints(x, p.getLocation().getBlockX(), z, p.getLocation().getBlockZ());
+                    if (distance > 200) {
+                        packet.getIntegers().write(0, Integer.valueOf((-100000 + ItemsIlegales.this.rng.nextInt(100000)) * 8));
+                        packet.getIntegers().write(2, Integer.valueOf((-100000 + ItemsIlegales.this.rng.nextInt(100000)) * 8));
+                    }
+                }
+            }
+        });
     }
 
     @Override
@@ -51,7 +87,21 @@ public final class ItemsIlegales extends JavaPlugin implements Listener {
 
     @EventHandler
     public void alPortalUsar(PlayerPortalEvent e) {
-        e.getTo().getWorld().getName()
+        if(e.getTo().getWorld().getEnvironment().equals(World.Environment.THE_END)){
+            Location l = e.getTo().getWorld().getSpawnLocation();
+            l.setX(100.0);
+            l.setY(51.0);
+            l.setZ(0.0);
+            e.getPlayer().teleport(e.getTo().getWorld().getSpawnLocation());
+        }
+    }
+
+
+    @EventHandler
+    public void romperEndPortal(BlockBreakEvent e){
+        if(e.getBlock().getType() == Material.END_PORTAL){
+            e.setCancelled(true);
+        }
     }
 
     @EventHandler
@@ -87,5 +137,10 @@ public final class ItemsIlegales extends JavaPlugin implements Listener {
             }
         }
         return false;
+    }
+
+
+    public int distanceBetweenPoints(int x1, int x2, int y1, int y2) {
+        return (int)Math.sqrt(((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2)));
     }
 }

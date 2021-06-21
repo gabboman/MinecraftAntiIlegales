95594e91fb747c28505306c2b6e693d69acff279
Gabriel Amador García <gabriel.amador.garcia@gmail.com>
2021-06-21 13:23:49 +0200

    removed danimania

--- src/main/java/com/gabboman/mineanarquia/itemsilegales/Mineanarquia/ItemsIlegales.java
+++ src/main/java/com/gabboman/mineanarquia/itemsilegales/Mineanarquia/ItemsIlegales.java
@@ -1,4 +1,4 @@
-package com.danimania.mineanarquia.itemsilegales.Mineanarquia;
+package com.gabboman.mineanarquia.itemsilegales.Mineanarquia;
 
 import com.comphenix.protocol.PacketType;
 import com.comphenix.protocol.ProtocolLibrary;

0134070dedc025f99c892e19f87e0c489081220b
nekiak <neekiak@gmail.com>
2021-07-02 11:31:35 -0300

    fix: improved style and cleaned unused imports

--- src/main/java/com/gabboman/mineanarquia/itemsilegales/Mineanarquia/ItemsIlegales.java
+++ src/main/java/com/gabboman/mineanarquia/itemsilegales/Mineanarquia/ItemsIlegales.java
@@ -6,7 +6,6 @@ import com.comphenix.protocol.ProtocolManager;
 import com.comphenix.protocol.events.PacketAdapter;
 import com.comphenix.protocol.events.PacketContainer;
 import com.comphenix.protocol.events.PacketEvent;
-import com.comphenix.protocol.events.PacketListener;
 import com.comphenix.protocol.reflect.StructureModifier;
 import com.google.common.collect.Multimap;
 import org.bukkit.*;
@@ -23,16 +22,13 @@ import org.bukkit.event.block.BlockDropItemEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.EntityPickupItemEvent;
-import org.bukkit.event.inventory.InventoryMoveItemEvent;
 import org.bukkit.event.inventory.InventoryOpenEvent;
 import org.bukkit.event.inventory.InventoryType;
 import org.bukkit.event.player.*;
 import org.bukkit.event.world.ChunkLoadEvent;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.BlockStateMeta;
 import org.bukkit.inventory.meta.FireworkMeta;
 import org.bukkit.inventory.meta.PotionMeta;
-import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.potion.PotionEffect;
 
@@ -45,9 +41,9 @@ public final class ItemsIlegales extends JavaPlugin implements Listener {
     private ProtocolManager protocolManager;
     private Set<Material> materialesIlegales;
     private Set<Material> stacksIlegales;
+
     public void onLoad() {
         protocolManager = ProtocolLibrary.getProtocolManager();
-
     }
 
     @Override
@@ -66,61 +62,55 @@ public final class ItemsIlegales extends JavaPlugin implements Listener {
         // vamos a quitar los spawner egg no?
 
         List<Material> materials = Arrays.asList(Material.values());
-        for (Material m: materials) {
+        for (Material m : materials) {
             // bloques ilegales. su existencia debe ser destruida
-            if (m.name().contains("SPAWN_EGG") || m.name().contains("COMMAND")
+            if (m.name().contains("SPAWN_EGG")
+                    || m.name().contains("COMMAND")
                     || m.name().contains("STRUCTURE")) {
                 materialesIlegales.add(m);
             }
 
-            if (m.name().contains("SWORD") || m.name().contains("BOOTS") ||
-                    m.name().contains("LEGGIN") || m.name().contains("CHESTPLATE") ||
-                    m.name().contains("HELMET") || m.name().contains("AXE") ||
-                    m.name().contains("HOE") || m.name().contains("SHIELD") ||
-                    m.name().contains("TOTEM") ) {
+            if (m.name().contains("SWORD")
+                    || m.name().contains("BOOTS")
+                    || m.name().contains("LEGGIN")
+                    || m.name().contains("CHESTPLATE")
+                    || m.name().contains("HELMET")
+                    || m.name().contains("AXE")
+                    || m.name().contains("HOE")
+                    || m.name().contains("SHIELD")
+                    || m.name().contains("TOTEM")) {
                 stacksIlegales.add(m);
             }
-
-            // stacks ilegales. si hay mas de uno deben ser destruidos
-
         }
 
         Bukkit.getServer().getPluginManager().registerEvents(this, this);
         // antithunder
-        this.protocolManager.addPacketListener((PacketListener)new PacketAdapter((Plugin)this, new PacketType[] { PacketType.Play.Server.NAMED_SOUND_EFFECT }) {
+        this.protocolManager.addPacketListener(new PacketAdapter(this, PacketType.Play.Server.NAMED_SOUND_EFFECT) {
             public void onPacketSending(PacketEvent event) {
-
                 // Sonidos que esten a mas de 250 del jugador seran cancelados? quien sabe
                 try {
                     final Player player = event.getPlayer();
                     PacketContainer packet = event.getPacket();
                     StructureModifier<Integer> ints = packet.getIntegers();
-                    int xPacket =  ints.read(0) / 8;
+                    int xPacket = ints.read(0) / 8;
                     int zPacket = ints.read(2) / 8;
                     double xPlayer = player.getLocation().getBlockX();
                     double zPlayer = player.getLocation().getBlockZ();
 
                     double xDif = Math.abs(xPlayer - xPacket);
                     double yDif = Math.abs(zPlayer - zPacket);
-                    if(xDif > 250 | yDif > 250) {
+                    if (xDif > 250 | yDif > 250) {
                         event.setCancelled(true);
                     }
-                }
-                catch(Exception e) {}
-
-
+                } catch (Exception e) { }
             }
         });
-
-
-
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
     }
-
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
@@ -135,8 +125,7 @@ public final class ItemsIlegales extends JavaPlugin implements Listener {
                 } else {
                     p.sendMessage(ChatColor.RED + "No tienes cama o nexo de reaparición.");
                 }
-            }
-            else if (label.equalsIgnoreCase("playtime") || label.equalsIgnoreCase("killstats")) {
+            } else if (label.equalsIgnoreCase("playtime") || label.equalsIgnoreCase("killstats")) {
                 if (args.length < 2) {
                     Player p = (Player) sender;
                     if (args.length == 1) {
@@ -162,9 +151,9 @@ public final class ItemsIlegales extends JavaPlugin implements Listener {
 
     // cuando se suelta un objeto (romper cofre etc)
     @EventHandler
-    public void dropear(BlockDropItemEvent e){
-        for(Item i : e.getItems()){
-            if(verificarIlegal(i.getItemStack())){
+    public void dropear(BlockDropItemEvent e) {
+        for (Item i : e.getItems()) {
+            if (verificarIlegal(i.getItemStack())) {
                 i.getItemStack().setAmount(0);
             }
         }
@@ -172,8 +161,8 @@ public final class ItemsIlegales extends JavaPlugin implements Listener {
 
     // creo que es TODOS los eventos del jugador. A lo mejor podemos limpiar mucho
     @EventHandler
-    public void alCogerObjeto(EntityPickupItemEvent e){
-        if(verificarIlegal(e.getItem().getItemStack())){
+    public void alCogerObjeto(EntityPickupItemEvent e) {
+        if (verificarIlegal(e.getItem().getItemStack())) {
             e.setCancelled(true);
             e.getItem().getItemStack().setAmount(0);
         }
@@ -181,34 +170,32 @@ public final class ItemsIlegales extends JavaPlugin implements Listener {
     }
 
     @EventHandler
-    public void jugadorInteractua(PlayerInteractEvent e){
-        if(verificarIlegal(e.getItem())) {
+    public void jugadorInteractua(PlayerInteractEvent e) {
+        if (verificarIlegal(e.getItem())) {
             e.setCancelled(true);
         }
         revisarJugador(e.getPlayer());
     }
 
     @EventHandler
-    public void alEntrar(PlayerJoinEvent e){
-        // verificarPocionesIlegales(e.getPlayer());
+    public void alEntrar(PlayerJoinEvent e) {
         revisarJugador(e.getPlayer());
-
     }
 
     @EventHandler
-    public void alCambioMano(PlayerSwapHandItemsEvent e){
+    public void alCambioMano(PlayerSwapHandItemsEvent e) {
         revisarJugador(e.getPlayer());
     }
 
     @EventHandler
     public void alPortalUsar(PlayerPortalEvent e) {
         revisarJugador(e.getPlayer());
-        if(e.getTo().getWorld().getEnvironment().equals(World.Environment.THE_END)){
-            if(e.getFrom().getWorld().getEnvironment().equals(World.Environment.NETHER)){
+        if (e.getTo().getWorld().getEnvironment().equals(World.Environment.THE_END)) {
+            if (e.getFrom().getWorld().getEnvironment().equals(World.Environment.NETHER)) {
                 e.setCancelled(true);
                 e.getPlayer().setHealth(1);
             }
-            if(Math.abs(e.getFrom().getBlockX()) > 25000.0 || Math.abs(e.getFrom().getBlockZ()) > 25000.0){
+            if (Math.abs(e.getFrom().getBlockX()) > 25000.0 || Math.abs(e.getFrom().getBlockZ()) > 25000.0) {
                 e.setCancelled(true);
                 e.getPlayer().setHealth(1);
                 // e.getPlayer().kickPlayer("tu portal no me gusta. En el futruo esto te matara");
@@ -216,29 +203,24 @@ public final class ItemsIlegales extends JavaPlugin implements Listener {
         }
     }
 
-
     @EventHandler
-    public void romperEndPortal(BlockBreakEvent e){
+    public void romperEndPortal(BlockBreakEvent e) {
         revisarJugador(e.getPlayer());
-        if(e.getBlock().getType() == Material.END_PORTAL){
+        if (e.getBlock().getType() == Material.END_PORTAL) {
             e.setCancelled(true);
         }
     }
-    
-    
+
     @EventHandler
     public void alMoverse(PlayerMoveEvent e) {
         final Player p = e.getPlayer();
         revisarPosicionJugador(p);
-
     }
-
 
     @EventHandler
     public void alColocarBloque(BlockPlaceEvent e) {
-
         ItemStack items = new ItemStack(e.getBlock().getType());
-        if(verificarIlegal(items)) {
+        if (verificarIlegal(items)) {
             e.setCancelled(true);
             e.getPlayer().setHealth(1);
         }
@@ -246,120 +228,108 @@ public final class ItemsIlegales extends JavaPlugin implements Listener {
     }
 
     @EventHandler
-    public void alAbrir(InventoryOpenEvent e){
+    public void alAbrir(InventoryOpenEvent e) {
         revisarJugador(e.getPlayer());
-        for(ItemStack i: e.getPlayer().getInventory()){
-            if (verificarIlegal(i)){
+        for (ItemStack i : e.getPlayer().getInventory()) {
+            if (verificarIlegal(i)) {
                 i.setAmount(0);
             }
         }
         boolean ilegales = false;
-        if(e.getInventory().getType() == InventoryType.HOPPER){
-            for(ItemStack it : e.getPlayer().getInventory().getContents()){
-                if(verificarIlegal(it)){
+        if (e.getInventory().getType() == InventoryType.HOPPER) {
+            for (ItemStack it : e.getPlayer().getInventory().getContents()) {
+                if (verificarIlegal(it)) {
                     it.setAmount((0));
                     ilegales = true;
                 }
             }
         }
-
-
-        for(ItemStack i : e.getInventory().getContents()){
-            if(verificarIlegal(i)){
+        for (ItemStack i : e.getInventory().getContents()) {
+            if (verificarIlegal(i)) {
                 ilegales = true;
                 i.setAmount(0);
             }
-
-
         }
-        if(ilegales){
-            Bukkit.getServer().getLogger().info("Items ilegales. Jugador: "+e.getPlayer().getName()+". X"+e.getPlayer().getLocation().getX()+", Y"+e.getPlayer().getLocation().getY()+", Z"+e.getPlayer().getLocation().getZ());
+        if (ilegales) {
+            Bukkit.getServer().getLogger().info("Items ilegales. Jugador: " + e.getPlayer().getName() + ". X" + e.getPlayer().getLocation().getX() + ", Y" + e.getPlayer().getLocation().getY() + ", Z" + e.getPlayer().getLocation().getZ());
             e.getPlayer().setHealth(1);
         }
-
     }
 
     @EventHandler
-    public void alCargarChunk(ChunkLoadEvent e){
-        if(!e.isNewChunk()){
-            for(Entity ent: e.getChunk().getEntities()) {
+    public void alCargarChunk(ChunkLoadEvent e) {
+        if (!e.isNewChunk()) {
+            for (Entity ent : e.getChunk().getEntities()) {
                 verificarEntidadIlegal(ent);
             }
         }
     }
 
     @EventHandler
-    public void alCausarDaño(EntityDamageEvent e){
-        if(e.getDamage() > 200){
+    public void alCausarDaño(EntityDamageEvent e) {
+        if (e.getDamage() > 200) {
             e.setCancelled(true);
         }
     }
 
-    public void verificarEntidadIlegal(Entity e ) {
+    public void verificarEntidadIlegal(Entity e) {
         Boolean nombreIlegal = false;
-        if(e.getCustomName() != null && e.getCustomName().toLowerCase().contains("danimania")){
+        if (e.getCustomName() != null && e.getCustomName().toLowerCase().contains("danimania")) {
             nombreIlegal = true;
         }
-        if((e.isInvulnerable() || nombreIlegal ) && e.getType() != EntityType.PLAYER  ) {
-            Bukkit.getServer().getLogger().warning("Posible entidad ilegal: "+ ". X"+e.getLocation().getX()+", Y"+e.getLocation().getY()+", Z"+e.getLocation().getZ());
+        if ((e.isInvulnerable() || nombreIlegal) && e.getType() != EntityType.PLAYER) {
+            Bukkit.getServer().getLogger().warning("Posible entidad ilegal: " + ". X" + e.getLocation().getX() + ", Y" + e.getLocation().getY() + ", Z" + e.getLocation().getZ());
             e.setInvulnerable(false);
-            e.teleport(new Location(e.getLocation().getWorld(), 0, -20 , 0));
+            e.teleport(new Location(e.getLocation().getWorld(), 0, -20, 0));
             e.remove();
-
         }
-
     }
 
-    public boolean verificarIlegal(ItemStack item){
+    public boolean verificarIlegal(ItemStack item) {
         // nombre ilegal ilegal!
         try {
-            if(item.getItemMeta().hasDisplayName()){
-                Multimap<Attribute, AttributeModifier> modificadores = item.getItemMeta().getAttributeModifiers();
+            if (item.getItemMeta().hasDisplayName()) {
+                Multimap<Attribute, AttributeModifier> modificadores =
+                        item.getItemMeta().getAttributeModifiers();
                 try {
-                    for ( Attribute attr: modificadores.asMap().keySet()) {
-                        for( AttributeModifier mod : modificadores.get(attr) ){
-                            if(mod.getAmount() > 50.0) {
+                    for (Attribute attr : modificadores.asMap().keySet()) {
+                        for (AttributeModifier mod : modificadores.get(attr)) {
+                            if (mod.getAmount() > 50.0) {
                                 return true;
                             }
                         }
                     }
-                } catch (Exception e) {
 
-                }
+                } catch (Exception e) { }
             }
+        } catch (Exception e) { }
 
-        } catch (Exception e){
-            
-        }
-
-        if(item != null){
-            if(item.getType() == Material.FIREWORK_ROCKET){
+        if (item != null) {
+            if (item.getType() == Material.FIREWORK_ROCKET) {
                 FireworkMeta fwm = (FireworkMeta) item.getItemMeta();
-                if(fwm.getPower()>3){
+                if (fwm.getPower() > 3) {
                     return true;
                 }
-            }else if(
-                    materialesIlegales.contains(item.getType())
-            ){
+            } else if (materialesIlegales.contains(item.getType())) {
                 return true;
-            }else if(item.getType() == Material.SPLASH_POTION){
+            } else if (item.getType() == Material.SPLASH_POTION) {
                 PotionMeta meta = (PotionMeta) item.getItemMeta();
-                for(PotionEffect pe : meta.getCustomEffects()){
-                    if(pe.getAmplifier()>4){
+                for (PotionEffect pe : meta.getCustomEffects()) {
+                    if (pe.getAmplifier() > 4) {
                         return true;
                     }
                 }
             }
 
-            if( stacksIlegales.contains(item.getType())){
-                if(item.getAmount()>1){
+            if (stacksIlegales.contains(item.getType())) {
+                if (item.getAmount() > 1) {
                     return true;
                 }
             }
 
-            Map <Enchantment, Integer> enchants = item.getEnchantments();
-            for (Enchantment e: enchants.keySet()) {
-                if (e.getMaxLevel() < enchants.get(e)){
+            Map<Enchantment, Integer> enchants = item.getEnchantments();
+            for (Enchantment e : enchants.keySet()) {
+                if (e.getMaxLevel() < enchants.get(e)) {
                     return true;
                 }
             }
@@ -367,33 +337,30 @@ public final class ItemsIlegales extends JavaPlugin implements Listener {
         return false;
     }
 
-    public void revisarJugador (HumanEntity p){
-        if(p.getAttribute(GENERIC_MAX_HEALTH).getValue()> 36.0) {
-            Bukkit.getServer().getLogger().info("Jugador "+ p.getName() + " Tenia salud maxima de " + p.getAttribute(GENERIC_MAX_HEALTH).getValue());
-
+    public void revisarJugador(HumanEntity p) {
+        if (p.getAttribute(GENERIC_MAX_HEALTH).getValue() > 36.0) {
+            Bukkit.getServer().getLogger().info("Jugador " + p.getName() + " Tenia salud maxima de " + p.getAttribute(GENERIC_MAX_HEALTH).getValue());
             p.setHealth(1.0);
             p.getInventory().clear();
             Location location = p.getLocation();
             location.setY(-1000.);
             p.teleport(location);
         }
-        for(ItemStack i : p.getInventory().getContents()){
-            if(verificarIlegal(i)){
-                p.setHealth(1.0);
-                i.setAmount(0);
-
-            }
-        }
-
-        //verificar EQUIPO
-        for(ItemStack i : p.getInventory().getArmorContents()){
-            if(verificarIlegal(i)){
+        for (ItemStack i : p.getInventory().getContents()) {
+            if (verificarIlegal(i)) {
                 p.setHealth(1.0);
                 i.setAmount(0);
             }
         }
+        // verificar EQUIPO
+        for (ItemStack i : p.getInventory().getArmorContents()) {
+            if (verificarIlegal(i)) {
+                p.setHealth(1.0);
+                i.setAmount(0);
+            }
+        }
 
-        if(verificarIlegal(p.getInventory().getItemInOffHand())){
+        if (verificarIlegal(p.getInventory().getItemInOffHand())) {
             p.setHealth(1.0);
             p.getInventory().getItemInOffHand().setAmount(0);
             // p.getInventory().clear();
@@ -411,13 +378,9 @@ public final class ItemsIlegales extends JavaPlugin implements Listener {
                 p.sendMessage("Advertencia: Sal del techo (o del vacío) antes de llegar a 1 millon.\n(O MORIRÁS!!!)");
             }
             if (x > 1000000 || z > 1000000) {
-                // voy a simplemente molestar al usuario mandandole mensajes
                 p.getInventory().clear();
                 p.setHealth(0.0);
             }
-
         }
     }
-
-
 }
